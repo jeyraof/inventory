e00e72ac7e592dcc4c4d9e65165d2388a7f7bab8
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-11-23 23:56:28 +0900

    Define approved webpack structure

--- inventory/static/js/vendor.js
+++ inventory/static/js/vendor.js
@@ -2,7 +2,8 @@
  * Created by Jaeyoung on 11/23/14.
  */
 
-var jquery = require('../vendor/jquery-1.11.1.min');
+// Jquery 1.11.1
+require('../vendor/jquery-1.11.1.min');
 
 // Normalize.css
-var normlize = require('../vendor/normalize.css');
+require('../vendor/normalize.css');
